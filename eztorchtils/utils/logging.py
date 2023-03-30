337bf2ba44eb952776966e22d2f14ae083b716be
ezhang7423 <ezhang7423@student.palomar.edu>
2023-03-30 09:55:15 -0700

    add inspect

--- eztorchtils/utils/logging.py
+++ eztorchtils/utils/logging.py
@@ -1,36 +1,57 @@
 from datetime import datetime
-
-SEPARATOR = "\n\n-----------------\n\n"
-
-
-class colors:
-    HEADER = "\033[95m"
-    OKBLUE = "\033[94m"
-    OKCYAN = "\033[96m"
-    OKGREEN = "\033[92m"
-    WARNING = "\033[93m"
-    FAIL = "\033[91m"
-    ENDC = "\033[0m"
-    BOLD = "\033[1m"
-    UNDERLINE = "\033[4m"
+from rich.console import Console
+from rich.tree import Tree
+import torch
+console = Console()
 
 
-def red(*args, **kwargs):
-    new_args = list(args)
-    new_args[0] = colors.FAIL + str(args[0])
-    print(*new_args, colors.ENDC, **kwargs)
+def print_creator(color):
+    def print_creator_inner(*args, **kwargs):
+        console.print(*args, **kwargs, style=color)        
+    return print_creator_inner
+
+red = print_creator("red")
+green = print_creator("green")
+bold = print_creator("bold")
+blue = print_creator("blue")
+purple = print_creator("purple")
+orange = print_creator("orange")
 
 
-def green(*args, **kwargs):
-    new_args = list(args)
-    new_args[0] = colors.OKGREEN + str(args[0])
-    print(*new_args, colors.ENDC, **kwargs)
+
+def inspect(obj):
+    console = Console(record=True)
+    
+    class TreeWrapper:
+        def __init__(self) -> None:
+            self.tree = None
+        def add(self, label):
+            if self.tree is None:
+                self.tree = Tree(label)
+                return self.tree
+            else:
+                return self.tree.add(label)
+            
+    def add_children(obj, root: Tree):
+        if isinstance(obj, dict):
+            dict_node = root.add(f'dict')
+            for key in obj.keys():
+                add_children(obj[key], dict_node.add(f"[bold]{key}[/bold]"))
+        elif isinstance(obj, list):
+            list_node = root.add(f'list[{len(obj)}]')
+            if len(obj) > 0:
+                add_children(obj[0], list_node)
+            if len(obj) > 1:
+                list_node.add('...')        
+        elif isinstance(obj, torch.Tensor):
+            root.add(f'tensor[[blue]{tuple(obj.shape)}[/blue]; {obj.dtype}]')
+        else:
+            root.add(str(type(obj)))
+    treewrapper = TreeWrapper()
+    add_children(obj,treewrapper )    
+    console.print(treewrapper.tree)
 
 
-def bold(*args, **kwargs):
-    new_args = list(args)
-    new_args[0] = colors.BOLD + str(args[0])
-    print(*new_args, colors.ENDC, **kwargs)
 
 
 def datestr(full=True):
